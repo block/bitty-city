058733032f50a2b1c8d2190abf93b3b4960d951a
Jackson Cook <jacksoncook73@gmail.com>
2026-02-05 13:54:18 -0800

    add deposit db errors (#98)

--- innie/src/main/kotlin/xyz/block/bittycity/innie/store/DepositEntityOperations.kt
+++ innie/src/main/kotlin/xyz/block/bittycity/innie/store/DepositEntityOperations.kt
@@ -3,6 +3,7 @@ package xyz.block.bittycity.innie.store
 import xyz.block.bittycity.innie.models.Deposit
 import xyz.block.bittycity.innie.models.DepositReversal
 import xyz.block.bittycity.innie.models.DepositToken
+import xyz.block.domainapi.InfoOnly
 
 interface DepositEntityOperations {
 
@@ -18,3 +19,22 @@ interface DepositEntityOperations {
 
   fun getLatestReversal(id: DepositToken): Result<DepositReversal?>
 }
+
+sealed class DepositStoreError(message: String) :
+  Exception(message),
+  InfoOnly
+
+class DepositNotPresent(val depositToken: DepositToken) :
+  DepositStoreError("Deposit not present: $depositToken")
+
+class DepositTokensEmpty : DepositStoreError("Deposit tokens not present")
+
+class TooManyDepositTokens(val depositTokenCount: Int, val depositTokenLimit: Int) :
+  DepositStoreError(
+    "Too many deposit tokens: $depositTokenCount, exceeded limit: $depositTokenLimit"
+  )
+
+class DepositVersionMismatch(val deposit: Deposit) :
+  DepositStoreError(
+    "Deposit not at expected version ${deposit.version}: ${deposit.id}"
+  )
