53f9aee169cede0b1f0719f354d5dce8f673da3b
Jackson Cook <jcook@squareup.com>
2026-02-10 16:13:43 -0800

    rotate wallet address to wallet client

--- innie/src/main/kotlin/xyz/block/bittycity/innie/client/WalletClient.kt
+++ innie/src/main/kotlin/xyz/block/bittycity/innie/client/WalletClient.kt
@@ -5,4 +5,12 @@ import xyz.block.bittycity.common.models.CustomerId
 
 interface WalletClient {
   fun lookupWallet(address: Address): Result<CustomerId?>
+
+  /**
+   * Rotates the wallet address for a given customer.
+   *
+   * @param customerId The ID of the customer whose wallet address should be rotated.
+   * @return a [Result] containing the new wallet [Address] if successful.
+   */
+  fun rotateWalletAddress(customerId: CustomerId): Result<Address>
 }
